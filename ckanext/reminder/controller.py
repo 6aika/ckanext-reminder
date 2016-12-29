c467c975003e0fa3f47dd6641b6c8c4d30a73ecb
Mikko Nieminen <mikko.nieminen@gofore.com>
2016-12-29 08:43:45 +0200

    Improve error handling when trying to subscribe to a package

--- ckanext/reminder/controller.py
+++ ckanext/reminder/controller.py
@@ -1,24 +1,29 @@
 import ckan.plugins as p
 import ckan.model as model
 import ckan.logic as logic
-from ckan.lib.base import h
+from ckan.lib.base import h, abort
 from ckan.common import request
 
 c = p.toolkit.c
+_ = p.toolkit._
 flatten_to_string_key = logic.flatten_to_string_key
 
 class ReminderController(p.toolkit.BaseController):
 
     def subscribe_to_package(self, package_id):
-        p.toolkit.get_action('subscribe_to_package')(
-            context = {'model': model,
-                       'user': c.user or c.author},
-            data_dict={'package': package_id,
-                       'subscriber_email': request.POST['subscriber_email']}
-        )
-        c.package_subscription_successful = True
-        h.redirect_to(str('/dataset/' + package_id))
-        return p.toolkit.render('package/read_base.html')
+
+        try:
+            p.toolkit.get_action('subscribe_to_package')(
+                context = {'model': model,
+                           'user': c.user or c.author},
+                data_dict={'package': package_id,
+                           'subscriber_email': request.POST['subscriber_email']}
+            )
+            c.package_subscription_successful = True
+            h.redirect_to(str('/dataset/' + package_id))
+            return p.toolkit.render('package/read_base.html')
+        except logic.ValidationError, error:
+            abort(400, _('Invalid request: {error_message}').format(error_message=str(error)))
 
     def unsubscribe_index(self, subscriber_email, unsubscribe_token):
         c.subscriptions = p.toolkit.get_action('get_packages_for_user')(
